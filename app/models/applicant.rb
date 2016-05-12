f992a46be5b3f9d2c4ab7e9357de5533f53f61d5
tuckerbohman5 <tucker.bohman@gmail.com>
2016-05-12 17:53:29 -0600

    general functionality is working

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -2,4 +2,13 @@ class Applicant < ActiveRecord::Base
   has_one :job
   has_many :skills
 
+  def first_skill
+    skills.first
+  end
+
+  def additional_skills
+    count = skills.size - 1
+    skills.last(count)
+  end
+
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,3 @@
 class Job < ActiveRecord::Base
-
+  has_many :applicants
 end
